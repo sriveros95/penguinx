41e2ad77b8d12e778bda4ed81ca1631caa7cb0b3
Gino <gagf50@gmail.com>
2022-12-21 20:10:02 -0500

    Changes in style

---
+++ app/pages/_app.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
+import Header from "../components/Header";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Goerli;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>penguinX</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Buy and sell stuff with crypto, no intermediaries, no fees! The P2P exchange, only for cool stuff. 😎"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+        />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <ThirdwebGuideFooter />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
---
+++ app/pages/index.tsx
@@ -0,0 +1,100 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import {
+  MediaRenderer,
+  useActiveListings,
+  useContract,
+} from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
+import { Marketplace } from "@thirdweb-dev/sdk";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  // Connect your marketplace smart contract here (replace this address)
+  const { contract: marketplace } = useContract(
+    "0xA82420CA51AcAF151351f14f718e2F74183cA58e", // Your marketplace contract address here
+    "marketplace"
+  );
+
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(marketplace);
+
+  return (
+    <>
+      {/* Content */}
+      <div className={styles.container}>
+        {/* Top Section */}
+        <h1 className={styles.h1}>Buy and sell stuff with crypto, <br/>no intermediaries, no fees!</h1>
+        <p className={styles.explain}>
+          The P2P exchange, only for{" "}
+          <b>
+            {" "}
+            <a
+              href="https://thirdweb.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.orange}
+            >
+              cool
+            </a>
+          </b>{" "}
+         stuff. 😎
+        </p>
+
+        <hr className={styles.divider} />
+
+        <div style={{ marginTop: 32, marginBottom: 32 }}>
+          <Link href="/create">
+            <a className={styles.mainButton} style={{ textDecoration: "none" }}>
+              Create A Listing
+            </a>
+          </Link>
+        </div>
+
+        <div className="main">
+          {
+            // If the listings are loading, show a loading message
+            loadingListings ? (
+              <div>Loading listings...</div>
+            ) : (
+              // Otherwise, show the listings
+              <div className={styles.listingGrid}>
+                {listings?.map((listing) => (
+                  <div
+                    key={listing.id}
+                    className={styles.listingShortView}
+                    onClick={() => router.push(`/listing/${listing.id}`)}
+                  >
+                    <MediaRenderer
+                      src={listing.asset.image}
+                      style={{
+                        borderRadius: 16,
+                        // Fit the image to the container
+                        width: "100%",
+                        height: "100%",
+                      }}
+                    />
+                    <h2 className={styles.nameContainer}>
+                      <Link href={`/listing/${listing.id}`}>
+                        <a className={styles.name}>{listing.asset.name}</a>
+                      </Link>
+                    </h2>
+
+                    <p>
+                      <b>{listing.buyoutCurrencyValuePerToken.displayValue}</b>{" "}
+                      {listing.buyoutCurrencyValuePerToken.symbol}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
