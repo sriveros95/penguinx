7be83dbceed2c72c72abc2ad544bd3f50e64d615
Gino <gagf50@gmail.com>
2022-12-27 22:59:16 -0500

    CHanges to upload file input style

--- app/pages/create.tsx
+++ app/pages/create.tsx
@@ -218,15 +218,22 @@ const Create: NextPage = () => {
               placeholder="Product Description"
             />
 
-            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-
             {/* Sale Price For Listing Field */}
-            {/* <input
+            <input
             type="text"
             name="price"
             className={styles.textInput}
             placeholder="Sale Price"
-          /> */}
+            />
+
+            <label htmlFor="file-upload" className={styles.uploadFile}>
+              <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+              Upload product image
+              <div>
+                <img src="upload.png" className={styles.uploadIcon} alt="upload"></img>
+              </div>
+            </label>
+            <input id="file-upload" type="file"/>
 
             <button
               type="submit"
