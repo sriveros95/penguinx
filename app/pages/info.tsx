0501c77ae37d499235fc49c15d2ef534e7c21932
Gino <gagf50@gmail.com>
2022-12-28 19:43:31 -0500

    Style changes

--- app/pages/info.tsx
+++ app/pages/info.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-        <h1 className={styles.sub2}>
-          Welcome to penguinX!
+        <h1 className={styles.h1}>
+          Welcome to 🐧penguinX!
         </h1>
 
         <p className={styles.info}>
