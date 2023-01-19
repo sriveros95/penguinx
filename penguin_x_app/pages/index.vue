eef9d732663a11746451c6cc512d222b9e98f0bd
Gino <gagf50@gmail.com>
2023-01-18 21:25:33 -0500

    Worked on index ui

--- penguin_x_app/pages/index.vue
+++ penguin_x_app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12" md="11">
+  <v-row justify="center" class="fullWidth">
+    <v-container fluid>
       <h1 class="h1">🐧 penguinX</h1>
       <p class="explain">
         The market for unique items, just like Etsy...with <span class="mainGradient">crypto</span>.<br/>Sign in with MetaMask.
@@ -16,6 +16,21 @@
           <v-btn to="/buy" class="mainButton mx-1">Shop Cool Stuff</v-btn>
         </v-col>
       </v-row>
-    </v-col>
+      <v-row class="left">
+        <p class="sub2 text-left">
+          Your listings
+        </p>
+      </v-row>
+      <v-row class="left">
+        <p class="sub2 text-left">
+          Unverified listings
+        </p>
+      </v-row>
+      <v-row class="left">
+        <p class="sub2 text-left">
+          Check out all the cool stuff
+        </p>
+      </v-row>
+    </v-container>
   </v-row>
 </template>
